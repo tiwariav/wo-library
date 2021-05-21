94f248eeae279066c13492dacf50d6caed2aab62
Arvind Tiwari <tiwariav@gmail.com>
2021-05-22 04:10:04 +0530

    feat: ObserveInView component

---
+++ components/ObserveInView/ObserveInView.jsx
@@ -0,0 +1,36 @@
+import clsx from "clsx";
+import React, { useEffect, useState } from "react";
+import { useInView } from "react-intersection-observer";
+import styles from "./observeInView.module.css";
+
+export default function ObserveInView({
+  className,
+  dynamicClasses,
+  observeOptions,
+  ...props
+}) {
+  const [rootRef, inView, rootEntry] = useInView(observeOptions);
+  const [rootClasses, setRootClasses] = useState();
+
+  useEffect(() => {
+    if (rootEntry) {
+      const classNameProps = {
+        [styles["in-view"]]: inView,
+        [styles["out-bottom"]]: rootEntry.boundingClientRect.top > 0,
+        [styles["out-top"]]: rootEntry.boundingClientRect.top < 0,
+      };
+      if (dynamicClasses && typeof dynamicClasses === "object") {
+        Object.entries(dynamicClasses).forEach(([key, value]) => {
+          classNameProps[value] = classNameProps[styles[key]];
+        });
+      }
+      setRootClasses(classNameProps);
+    }
+  }, [inView, dynamicClasses, rootEntry]);
+
+  return (
+    <div ref={rootRef}>
+      <div className={clsx(styles.root, rootClasses)} {...props} />
+    </div>
+  );
+}
--- components/index.js
+++ components/index.js
@@ -1,1 +1,2 @@
+export { default as ObserveInView } from "./ObserveInView/ObserveInView";
 export { default as WoSwiper } from "./WoSwiper/WoSwiper";
