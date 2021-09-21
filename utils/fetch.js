e7f33ac6ccbbd91322a6c7e00bc32151b2b8dbc2
Prateek Gupta <prateekgupta@Prateeks-MacBook-Air.local>
2021-09-21 11:21:02 +0530

    refactor: storae type

--- utils/fetch.js
+++ utils/fetch.js
@@ -1,21 +1,21 @@
-import { merge } from "lodash-es";
-import { anyStorage } from "../lib/storage";
-import { WoErrorData, WoNetworkError, WoResponseError } from "./error";
+import { merge } from 'lodash-es';
+import { AnyStorage } from '../lib/storage';
+import { WoErrorData, WoNetworkError, WoResponseError } from './error';
 
-const ACCESS_TOKEN = "wo:authToken";
-const CONTENT_TYPE_FORM = "multipart/form-data";
+const ACCESS_TOKEN = 'wo:authToken';
+const CONTENT_TYPE_FORM = 'multipart/form-data';
 const DEFAULT_HEADERS = {
-  "Content-Type": "application/json"
-}
+  'Content-Type': 'application/json',
+};
 
 function getFormData(data, file) {
   const formData = new FormData();
   if (data) {
     for (const [key, value] of Object.entries(data)) {
-      formData.append(key, value)
+      formData.append(key, value);
     }
   }
-  formData.append("file", file);
+  formData.append('file', file);
   return formData;
 }
 
@@ -23,19 +23,19 @@ export function defaultResponseErrorHandler(error, data) {
   // handle http response status codes
   switch (data.status) {
     case 400:
-      throw new WoErrorData(error, data, "Invalid Data!");
+      throw new WoErrorData(error, data, 'Invalid Data!');
     case 401:
-      throw new WoErrorData(error, data, "You session has expired!");
+      throw new WoErrorData(error, data, 'You session has expired!');
     case 403:
       throw new WoErrorData(
         error,
         data,
-        "You are not authorized to access this page!"
+        'You are not authorized to access this page!',
       );
     case 404:
-      throw new WoErrorData(error, data, "Endpoint not found!");
+      throw new WoErrorData(error, data, 'Endpoint not found!');
     case 500:
-      throw new WoErrorData(error, data, "Internal server error!");
+      throw new WoErrorData(error, data, 'Internal server error!');
     default:
       break;
   }
@@ -44,53 +44,53 @@ export function defaultResponseErrorHandler(error, data) {
 export function defaultErrorHandler(error, data) {
   // handle manual set error status codes
   switch (error.name) {
-    case "TypeError":
-      throw new WoErrorData(error, data, "Response is not proper json!");
-    case "AbortError":
-      throw new WoErrorData(error, data, "Request aborted!");
-    case "WoResponseError":
+    case 'TypeError':
+      throw new WoErrorData(error, data, 'Response is not proper json!');
+    case 'AbortError':
+      throw new WoErrorData(error, data, 'Request aborted!');
+    case 'WoResponseError':
       defaultResponseErrorHandler(error, data);
       break;
     default:
-      throw new WoErrorData(error, data, "Unknown Error!");
+      throw new WoErrorData(error, data, 'Unknown Error!');
   }
 }
 
 export function combineURLs(baseURL, relativeURL) {
   const combinedUrl = relativeURL
-    ? `${baseURL.replace(/\/+$/, "")}/${relativeURL.replace(/^\/+/, "")}`
+    ? `${baseURL.replace(/\/+$/, '')}/${relativeURL.replace(/^\/+/, '')}`
     : baseURL;
-  return combinedUrl.replace(/\/+$/, "");
+  return combinedUrl.replace(/\/+$/, '');
 }
 
 export function createURL(
   apiEndpoint,
   path,
-  { query, id, trailingSlash, devProxy } = {}
+  { query, id, trailingSlash, devProxy } = {},
 ) {
   let resourceURL = path;
   let baseURL = apiEndpoint;
   if (devProxy && path.startsWith(devProxy)) {
-    resourceURL = path.replace(devProxy, "");
+    resourceURL = path.replace(devProxy, '');
   }
-  if (resourceURL.startsWith("http:") || resourceURL.startsWith("https:")) {
+  if (resourceURL.startsWith('http:') || resourceURL.startsWith('https:')) {
     resourceURL = new URL(combineURLs(resourceURL));
   } else {
     if (
-      !(apiEndpoint.startsWith("http:") || apiEndpoint.startsWith("https:"))
+      !(apiEndpoint.startsWith('http:') || apiEndpoint.startsWith('https:'))
     ) {
       baseURL = combineURLs(window.location.origin, apiEndpoint);
     }
     resourceURL = new URL(combineURLs(baseURL, resourceURL));
   }
-  if (id && resourceURL.pathname.slice(-1) !== "/") {
+  if (id && resourceURL.pathname.slice(-1) !== '/') {
     resourceURL.pathname += `/${id}`;
   }
   if (query) {
     resourceURL.search = new URLSearchParams(query);
   }
-  if (trailingSlash && resourceURL.pathname.slice(-1) !== "/") {
-    resourceURL.pathname += "/";
+  if (trailingSlash && resourceURL.pathname.slice(-1) !== '/') {
+    resourceURL.pathname += '/';
   }
   return resourceURL;
 }
@@ -101,11 +101,11 @@ export class WoFetch {
     {
       tokenName = ACCESS_TOKEN,
       errorHandler = defaultErrorHandler,
-      authHeader = "Authorization",
-      authTokenPrefix = "Bearer",
+      authHeader = 'Authorization',
+      authTokenPrefix = 'Bearer',
       trailingSlash = false,
       devProxy,
-    } = {}
+    } = {},
   ) {
     this.apiEndpoint = apiEndpoint;
     this.tokenName = tokenName;
@@ -122,20 +122,20 @@ export class WoFetch {
         // success, but no response content
         return response.text();
       }
-      const contentType = response.headers.get("content-type");
-      if (contentType && contentType.includes("application/json")) {
+      const contentType = response.headers.get('content-type');
+      if (contentType && contentType.includes('application/json')) {
         return response.json();
       }
       return response;
     }
-    return errorHandler(new WoResponseError("Not Ok!"), response);
+    return errorHandler(new WoResponseError('Not Ok!'), response);
   };
 
   handleError = async (error, errorHandler = this.errorHandler) => {
     if (error.data) {
       throw error;
     } else {
-      return errorHandler(new WoNetworkError("Network Error!"), error);
+      return errorHandler(new WoNetworkError('Network Error!'), error);
     }
   };
 
@@ -147,11 +147,11 @@ export class WoFetch {
   } = {}) => {
     const headersInstance = xhr ? new Map() : new Headers();
     const allHeaders = merge({}, DEFAULT_HEADERS, headers);
-    if (allHeaders["Content-Type"] === CONTENT_TYPE_FORM) {
-      delete allHeaders["Content-Type"];
+    if (allHeaders['Content-Type'] === CONTENT_TYPE_FORM) {
+      delete allHeaders['Content-Type'];
     }
     if (requireAuth && this.tokenName) {
-      const accessToken = token || anyStorage.getItem(this.tokenName);
+      const accessToken = token || new AnyStorage().getItem(this.tokenName);
       allHeaders[this.authHeader] = `${this.authTokenPrefix} ${accessToken}`;
     }
     for (const key in allHeaders) {
@@ -178,7 +178,7 @@ export class WoFetch {
       requireAuth,
       trailingSlash = this.trailingSlash,
       token,
-    } = {}
+    } = {},
   ) => {
     const requestHeaders = this.getHeaders({ headers, requireAuth, token });
     const url = createURL(this.apiEndpoint, path, {
@@ -190,22 +190,24 @@ export class WoFetch {
     let body;
     if (data) {
       body =
-        requestHeaders.get("Content-Type") === CONTENT_TYPE_FORM ? data : JSON.stringify(data);
+        requestHeaders.get('Content-Type') === CONTENT_TYPE_FORM
+          ? data
+          : JSON.stringify(data);
     }
     return fetch(url, { body, headers: requestHeaders, method })
-      .then((response) => this.handleResponse(response, errorHandler))
-      .catch((error) => this.handleError(error, errorHandler));
+      .then(response => this.handleResponse(response, errorHandler))
+      .catch(error => this.handleError(error, errorHandler));
   };
 
-  getUrl = async (...args) => this.fetchURL("GET", ...args);
+  getUrl = async (...args) => this.fetchURL('GET', ...args);
 
-  postUrl = async (...args) => this.fetchURL("POST", ...args);
+  postUrl = async (...args) => this.fetchURL('POST', ...args);
 
-  putUrl = async (...args) => this.fetchURL("PUT", ...args);
+  putUrl = async (...args) => this.fetchURL('PUT', ...args);
 
-  patchUrl = async (...args) => this.fetchURL("PATCH", ...args);
+  patchUrl = async (...args) => this.fetchURL('PATCH', ...args);
 
-  deleteUrl = async (...args) => this.fetchURL("DELETE", ...args);
+  deleteUrl = async (...args) => this.fetchURL('DELETE', ...args);
 
   uploadFileXHR = (
     path,
@@ -217,12 +219,12 @@ export class WoFetch {
       loadStartFunction,
       transferCompleteFunction,
       onStateChange,
-    }
+    },
   ) => {
-    const formData = getFormData(data, file)
+    const formData = getFormData(data, file);
 
     const headers = this.getHeaders({
-      headers: { "Content-Type": CONTENT_TYPE_FORM },
+      headers: { 'Content-Type': CONTENT_TYPE_FORM },
       xhr: true,
       requireAuth,
     });
@@ -232,17 +234,25 @@ export class WoFetch {
     return new Promise((resolve, reject) => {
       // add event listeners to xhrObj
       if (loadStartFunction) {
-        xhrObject.upload.addEventListener("loadstart", loadStartFunction, false);
+        xhrObject.upload.addEventListener(
+          'loadstart',
+          loadStartFunction,
+          false,
+        );
       }
       if (progressFunction) {
-        xhrObject.upload.addEventListener("progress", progressFunction, false);
+        xhrObject.upload.addEventListener('progress', progressFunction, false);
       }
       if (transferCompleteFunction) {
-        xhrObject.upload.addEventListener("load", transferCompleteFunction, false);
+        xhrObject.upload.addEventListener(
+          'load',
+          transferCompleteFunction,
+          false,
+        );
       }
 
       // when an XHR object is opened, add a listener for its readystatechange events
-      xhrObject.addEventListener("readystatechange", (e) => {
+      xhrObject.addEventListener('readystatechange', e => {
         if (onStateChange) {
           onStateChange(xhrObject.readyState, xhrObject);
         }
@@ -256,15 +266,15 @@ export class WoFetch {
       });
 
       xhrObject.open(
-        "POST",
+        'POST',
         createURL(this.apiEndpoint, path, {
           trailingSlash: this.trailingSlash,
           devProxy: this.devProxy,
         }),
-        true
+        true,
       );
       for (const key of Object.keys(headers)) {
-        xhrObject.setRequestHeader(key, headers[key])
+        xhrObject.setRequestHeader(key, headers[key]);
       }
       xhrObject.send(formData);
     });
