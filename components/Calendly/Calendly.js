d0295e405901ba60a1b1805bfd22d68c4b007afa
Arvind Tiwari <tiwariav@gmail.com>
2021-10-13 07:56:32 +0530

    feat: add calendly integration component

---
+++ components/Calendly/Calendly.js
@@ -0,0 +1,33 @@
+import { useEffectOnce } from "react-use";
+import { Button } from "ye-ui/components/atoms/forms";
+import loadScript from "../../lib/loadScript";
+import loadStylesheet from "../../lib/loadStylesheet";
+import { WoLoadScriptError } from "../../utils/error";
+
+export default function Calendly({ onClick, calendlyLink, prefill, ...props }) {
+  useEffectOnce(async () => {
+    const calendlyScript = await loadScript(
+      "https://assets.calendly.com/assets/external/widget.js"
+    );
+    const calendlyStlyesheet = await loadStylesheet(
+      "https://assets.calendly.com/assets/external/widget.css"
+    );
+
+    if (!(calendlyScript && calendlyStlyesheet)) {
+      throw new WoLoadScriptError("Calendly SDK failed to load!");
+    }
+    window.Calendly.initPopupWidget({
+      url: calendlyLink,
+      prefill,
+    });
+  });
+
+  const handleClick = (event) => {
+    window.Calendly.showPopupWidget(calendlyLink);
+    if (onClick) {
+      onClick(event);
+    }
+  };
+
+  return <Button onClick={handleClick} {...props} />;
+}
--- components/index.js
+++ components/index.js
@@ -1,2 +1,3 @@
+export { default as Calendly } from "./Calendly/Calendly";
 export { default as ObserveInView } from "./ObserveInView/ObserveInView";
 export { default as WoSwiper } from "./WoSwiper/WoSwiper";
