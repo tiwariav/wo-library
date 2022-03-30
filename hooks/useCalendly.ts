84a77a05fefcc3809cf07a2f63cd0f45418eaf21
Arvind Tiwari <tiwariav@gmail.com>
2022-03-30 21:06:50 +0530

    refactor: remove async function call from useCalendly hook

--- hooks/useCalendly.ts
+++ hooks/useCalendly.ts
@@ -2,18 +2,22 @@ import { useEffectOnce } from "react-use";
 import { loadScript, loadStylesheet } from "../lib";
 import { WoLoadScriptError } from "../utils/error";
 
-export default function useCalendly({ calendlyLink, prefill }) {
-  useEffectOnce(async () => {
-    const calendlyScript = await loadScript(
-      "https://assets.calendly.com/assets/external/widget.js"
-    );
-    const calendlyStlyesheet = await loadStylesheet(
-      "https://assets.calendly.com/assets/external/widget.css"
-    );
+async function loadCalendlyAssets() {
+  const calendlyScript = await loadScript(
+    "https://assets.calendly.com/assets/external/widget.js"
+  );
+  const calendlyStlyesheet = await loadStylesheet(
+    "https://assets.calendly.com/assets/external/widget.css"
+  );
 
-    if (!(calendlyScript && calendlyStlyesheet)) {
-      throw new WoLoadScriptError("Calendly SDK failed to load!");
-    }
+  if (!(calendlyScript && calendlyStlyesheet)) {
+    throw new WoLoadScriptError("Calendly SDK failed to load!");
+  }
+}
+
+export default function useCalendly({ calendlyLink, prefill }) {
+  useEffectOnce(() => {
+    loadCalendlyAssets();
   });
 
   return () => {
