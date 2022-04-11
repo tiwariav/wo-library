d68288108b1f550830693b182ebe980298753b58
Prateek Gupta <prateekgupta@ip-192-168-1-3.ap-south-1.compute.internal>
2022-04-11 19:43:29 +0530

    fix: removed root css

--- components/atoms/floating/Popover.tsx
+++ components/atoms/floating/Popover.tsx
@@ -1,20 +1,13 @@
 import clsx from "clsx";
-import styles from "./popover.module.css";
 import Tooltip from "./Tooltip";
 
 type Props = {
   content: React.ReactNode;
   className: string;
-  
-}
+};
 
-export default function Popover({ content, className, ...props }:Props) {
+export default function Popover({ content, className, ...props }: Props) {
   return (
-    <Tooltip
-      className={clsx(styles.root, className)}
-      title={content}
-      popover
-      {...props}
-    />
+    <Tooltip className={clsx(className)} title={content} popover {...props} />
   );
 }
