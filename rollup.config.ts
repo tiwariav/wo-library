7317f770da1544469c95c3e4b71f4c2ca9f46944
Arvind Tiwari <tiwariav@gmail.com>
2023-01-23 03:08:31 +0530

    feat: use rollup prrservemodules

--- rollup.config.ts
+++ rollup.config.ts
@@ -8,15 +8,15 @@ import postcssImport from "postcss-import";
 import postcssNormalize from "postcss-normalize";
 import postcssPresetEnv from "postcss-preset-env";
 import type { OutputOptions, RollupOptions } from "rollup";
-import autoExternal from "rollup-plugin-auto-external";
 import _copy from "rollup-plugin-copy";
 import _del from "rollup-plugin-delete";
-import _postcss from "rollup-plugin-postcss";
+import _externals from "rollup-plugin-node-externals";
+import _postcss, { PostCSSPluginConf } from "rollup-plugin-postcss";
 import progress from "rollup-plugin-progress";
-import { walk, walkIndex } from "./src/tools/files/index.js";
 // import { sizeSnapshot } from "rollup-plugin-size-snapshot";
 import { terser } from "rollup-plugin-terser";
 import _visualizer from "rollup-plugin-visualizer";
+import { rollupInputMap } from "./src/tools/files.js";
 
 const commonjs = defaultImport(_commonjs);
 const postcss = defaultImport(_postcss);
@@ -25,98 +25,89 @@ const del = defaultImport(_del);
 const typescript = defaultImport(_typescript);
 const beep = defaultImport(_beep);
 const visualizer = defaultImport(_visualizer);
+const externals = defaultImport(_externals);
 
 const isDev = Boolean(process.env.ROLLUP_WATCH);
-const plugins = [
+export const commonPlugins = [
   // sizeSnapshot(),
   progress(),
-  autoExternal(),
+  externals(),
   commonjs(),
 ];
+export const miscPlugins = isDev ? [beep(), visualizer()] : [terser()];
+export const tsPlugins = [
+  copy({
+    targets: [
+      { dest: "dist/assets", src: "assets/**/*" },
+      { dest: "dist", src: ["package.json", "README.md"] },
+    ],
+  }),
+  del({ runOnce: isDev, targets: "dist/**/*" }),
+  typescript({ tsconfig: "tsconfig.rollup.json" }),
+];
 
-const output: OutputOptions = {
+export const postcssConfig: PostCSSPluginConf = {
+  config: false,
+  extensions: [".css"],
+  extract: "dist.css",
+  modules: { localsConvention: "camelCase" },
+  plugins: [
+    cssnano({ preset: "default" }),
+    postcssImport(),
+    postcssFlexbugsFixes(),
+    postcssPresetEnv({
+      autoprefixer: {
+        flexbox: "no-2009",
+      },
+      features: {
+        "custom-media-queries": true,
+        "custom-properties": true,
+        "gap-properties": true,
+        "nesting-rules": true,
+      },
+      stage: 1,
+    }),
+    // Adds PostCSS Normalize as the reset css with default options,
+    // so that it honors browserslist config in package.json
+    // which in turn let's users customize the target behavior as per their needs.
+    postcssNormalize(),
+  ],
+  sourceMap: isDev,
+};
+
+export const esOutputOptions: OutputOptions = {
   chunkFileNames: "chunks/[name]-[hash].js",
-  dir: "./lib",
+  dir: "./dist",
   entryFileNames: "[name].js",
   format: "es",
   minifyInternalExports: !isDev,
   sourcemap: isDev,
 };
 
+export const cjsOutputOptions: OutputOptions = {
+  ...esOutputOptions,
+  entryFileNames: "[name].cjs",
+  exports: "auto",
+  format: "cjs",
+};
+
 const config: RollupOptions[] = [
   {
-    input: {
-      components: "src/components/index.ts",
-      "components/atoms": "src/components/atoms/index.ts",
-      "components/molecules": "src/components/molecules/index.ts",
-      // "components/templates": "src/components/templates/index.ts",
-      contexts: "src/contexts/index.ts",
-      hooks: "src/hooks/index.ts",
-      tools: "src/tools/index.ts",
-      ...walkIndex("src/tools"),
-      ...walk("src/components"),
-    },
-    output,
+    input: rollupInputMap(import.meta.url, "src"),
+    output: esOutputOptions,
     perf: isDev,
     plugins: [
-      ...plugins,
-      postcss({
-        config: false,
-        extensions: [".css"],
-        extract: "dist.css",
-        modules: { localsConvention: "camelCase" },
-        plugins: [
-          cssnano({ preset: "default" }),
-          postcssImport(),
-          postcssFlexbugsFixes(),
-          postcssPresetEnv({
-            autoprefixer: {
-              flexbox: "no-2009",
-            },
-            features: {
-              "custom-media-queries": true,
-              "custom-properties": true,
-              "gap-properties": true,
-              "nesting-rules": true,
-            },
-            stage: 1,
-          }),
-          // Adds PostCSS Normalize as the reset css with default options,
-          // so that it honors browserslist config in package.json
-          // which in turn let's users customize the target behavior as per their needs.
-          postcssNormalize(),
-        ],
-        sourceMap: isDev,
-      }),
-      copy({
-        targets: [
-          { dest: "lib/assets", src: "assets/**/*" },
-          { dest: "lib", src: ["package.json", "README.md"] },
-        ],
-      }),
-      del({ runOnce: isDev, targets: "lib/**/*" }),
-      typescript({
-        tsconfig: "tsconfig.rollup.json",
-      }),
-      ...(isDev ? [beep(), visualizer()] : [terser()]),
+      ...commonPlugins,
+      postcss(postcssConfig),
+      ...tsPlugins,
+      ...miscPlugins,
     ],
   },
   {
-    input: {
-      ...walk("src/tools/cjs", {
-        extensions: ["cjs"],
-        includeDirectories: true,
-      }),
-    },
-
-    output: {
-      ...output,
-      entryFileNames: "[name].cjs",
-      exports: "auto",
-      format: "cjs",
-    },
+    input: rollupInputMap(import.meta.url, "src", "*.cjs"),
+    output: cjsOutputOptions,
     perf: isDev,
-    plugins: [...plugins, ...(isDev ? [beep(), visualizer()] : [terser()])],
+    plugins: [...commonPlugins, ...miscPlugins],
   },
 ];
 
