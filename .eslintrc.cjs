365c6465cb1bf4698d0fda9858980ffb454ca7f9
Arvind Tiwari <tiwariav@gmail.com>
2023-06-13 18:42:12 +0530

    refactor: update packages

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,6 @@
 /* eslint-disable unicorn/prefer-module */
-const pkg = require("./src/tools/cjs/eslint.cjs");
-const { config: sharedConfig } = pkg;
+const eslintConfig = require("./src/tools/cjs/eslint.cjs");
+const { config: sharedConfig } = eslintConfig;
 
 sharedConfig.overrides[0].parserOptions = {
   project: ["./tsconfig.json"],
