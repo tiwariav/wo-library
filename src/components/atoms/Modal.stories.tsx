1e40d425f54d6d6bc594205c1cf286ec5bc314db
Aashish Singla <aashishsingla@ip-192-168-100-170.ap-south-1.compute.internal>
2023-08-05 23:02:32 +0530

    fix: prevent modal from overflowing the screen

--- src/components/atoms/Modal.stories.tsx
+++ src/components/atoms/Modal.stories.tsx
@@ -35,6 +35,55 @@ const Template = ({ isOpen = false, modalContent = "", ...args }) => {
   );
 };
 
+const MaxWidthTemplate = ({ isOpen = false, ...args }) => {
+  const [open, setOpen] = useState(isOpen);
+
+  useEffect(() => {
+    setOpen(isOpen);
+  }, [isOpen]);
+
+  return (
+    <div>
+      <button id="showButton" onClick={() => setOpen(true)}>
+        Show Modal
+      </button>
+      <Modal isOpen={open} onClose={() => setOpen(false)} {...args}>
+        <div className="story-box">
+          <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro
+            dolorum numquam id voluptatum labore, veniam, aspernatur accusamus
+            maxime odio hic sed est dicta voluptatibus nisi. Dolor temporibus
+            facere omnis culpa. Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Accusamus, officia? Sequi, corporis repellat,
+            tenetur itaque eius accusamus explicabo ex laboriosam dolore, nam
+            dicta debitis maxime excepturi! Sequi commodi quis cumque obcaecati.
+            Rerum molestias saepe quia minima earum quis cumque error expedita
+            aliquid quod optio repellat voluptate voluptatibus officia, eligendi
+            tempora neque illo sit hic ullam deleniti in vel consequuntur. Hic
+            veniam nihil sed esse. Officiis saepe veniam dicta quaerat quam
+            quasi omnis illum. Mollitia doloribus, voluptas tempora numquam
+            doloremque voluptatem deleniti blanditiis nisi ad, velit ipsum modi
+            cupiditate. In sint, unde delectus, laudantium tenetur placeat
+            tempora provident officia pariatur sapiente fuga tempore quia odit,
+            deserunt porro illum voluptatibus facere quam. Eum iste porro harum
+            rerum magnam. Vero magnam laborum accusantium nisi, nihil
+            exercitationem quas dolorum nostrum, sunt nobis neque aperiam,
+            numquam repellendus unde accusamus rem dolore alias minima eos
+            impedit odit quisquam quod. Necessitatibus, distinctio quisquam
+            tempora enim rem nam optio nostrum quae ipsum corporis, earum
+            aperiam, nemo a ex magnam modi praesentium excepturi hic dicta
+            inventore id consequatur totam nesciunt. Perspiciatis corporis
+            alias, odit officiis nostrum rem fugiat, excepturi autem animi quos
+            cumque ipsa ullam, eos illo. Officia ab harum quis nulla pariatur
+            neque optio? Modi culpa sed ipsam?
+            <input placeholder="Placeholder" />
+          </div>
+        </div>
+      </Modal>
+    </div>
+  );
+};
+
 export const Basic: Story = {
   play: async ({ canvasElement }) => {
     await playFloatingBasic(canvasElement, MODAL_TEXT);
@@ -55,4 +104,8 @@ export const OnClose: Story = {
     onClose: () => alert("Modal closed!"),
   },
   render: Basic.render,
+};
+
+export const MaxWidth: Story = {
+  render: (args) => <MaxWidthTemplate {...args} />,
 };
