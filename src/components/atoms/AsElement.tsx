2bc3c77f04d93d12c6193f6faafb90c2035ef199
Arvind Tiwari <tiwariav@gmail.com>
2023-10-18 02:58:22 +0530

    feat: add generic element wrapper AsElement

---
+++ src/components/atoms/AsElement.tsx
@@ -0,0 +1,13 @@
+import { ComponentPropsWithoutRef, ElementType } from "react";
+
+interface AsElementProps<TElement extends ElementType> {
+  as?: TElement;
+}
+
+export default function AsElement<TElement extends ElementType = "div">({
+  as,
+  ...props
+}: AsElementProps<TElement> & ComponentPropsWithoutRef<TElement>) {
+  const Element = as || "div";
+  return <Element {...props} />;
+}
