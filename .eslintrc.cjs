d12558267a4192db982832e23c3690200c220a51
Arvind Tiwari <tiwariav@gmail.com>
2023-11-21 03:02:26 +0530

    chore: udpate packages

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,9 +1,9 @@
 /* eslint-disable unicorn/prefer-module */
-const eslintConfig = require("./src/tools/cjs/eslint.cjs");
-const { config: sharedConfig } = eslintConfig;
 
-sharedConfig.overrides[0].parserOptions = {
-  project: ["./tsconfig.json"],
-  tsconfigRootDir: __dirname,
+module.exports = {
+  extends: ["@tiwariav"],
+  parserOptions: {
+    project: ["./tsconfig.json"],
+    tsconfigRootDir: __dirname,
+  },
 };
-module.exports = sharedConfig;
