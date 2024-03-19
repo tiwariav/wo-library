5571b86c7ad76b6112c9b6fae2b6c3bbdd5756c7
Arvind Tiwari <tiwariav@gmail.com>
2024-03-19 12:42:01 +0530

    refactor: update dependencies

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -7,9 +7,7 @@ import {
 
 const config: StorybookConfig = {
   ...main,
-  webpackFinal: (config, { configType }) => {
-    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
-    // 'PRODUCTION' is used when building the static version of storybook.
+  webpackFinal: (config) => {
     config = modulesFullySpecified(config);
     config = nodeNextExtensionAlias(config);
     return config;
