475a3540603aae9ea7ec4a685dea43a6d11f1bee
Arvind Tiwari <tiwariav@gmail.com>
2024-03-29 15:15:51 +0530

    refactor: improve storage getItem type based on json option

--- src/tools/storage/index.ts
+++ src/tools/storage/index.ts
@@ -2,7 +2,7 @@ export interface StorageBackend {
   clear: () => void;
   getItem: (key: string) => Promise<null | string> | null | string;
   removeItem: (key: string) => Promise<void> | void;
-  setItem: (key: string, value: string) => void;
+  setItem: (key: string, value: string) => Promise<void> | void;
 }
 
 export const STORAGE_ENVIRONMENTS = ["mobile", "server", "web"] as const;
@@ -12,103 +12,34 @@ export let memoryStorageItems: Record<string, null | string> = {};
 
 export const memoryStorage = {
   clear: () => (memoryStorageItems = {}),
-  // eslint-disable-next-line @typescript-eslint/require-await
-  getItem: async (key: string): Promise<null | string> =>
-    memoryStorageItems[key] ?? null,
+  getItem: (key: string): null | string => memoryStorageItems[key] ?? null,
   removeItem: (key: string) => {
     memoryStorageItems[key] = null;
   },
-  setItem: (key: string, value: string) => (memoryStorageItems[key] = value),
+  setItem: (key: string, value: string) => {
+    memoryStorageItems[key] = value;
+  },
 };
 
 type StorageBackendOptions = {
   [key in (typeof STORAGE_TYPES)[number]]: StorageBackend;
 };
+
+interface GetBackendOptions {
+  persist?: boolean;
+  session?: boolean;
+}
+
+interface GetItemOptions extends GetBackendOptions {
+  json?: boolean;
+}
+
 export class AnyStorage {
   backends: StorageBackendOptions;
-  clear = async () => {
-    this.backends.temp.clear();
-    if (this.prefix) {
-      for (const key of Object.keys(this.backends.session)) {
-        await this.backends.session.removeItem(key);
-      }
-      for (const key of Object.keys(this.backends.persist)) {
-        await this.backends.persist.removeItem(key);
-      }
-    }
-  };
   env: (typeof STORAGE_ENVIRONMENTS)[number];
   formKey = (key: string) => (this.prefix ? `${this.prefix}.${key}` : key);
-  getBackend = (persist: boolean, session: boolean) => {
-    if (persist) {
-      return this.backends.persist;
-    }
-    if (session) {
-      return this.backends.session;
-    }
-    return this.backends.temp;
-  };
-
-  getItem = async <TResponse extends null | string>(
-    key: string,
-    { json = this.json, persist = false, session = false } = {},
-  ): Promise<TResponse | null | string> => {
-    const storageKey = this.formKey(key);
-    const backend = this.getBackend(persist, session);
-    let response: TResponse | null | string = await backend.getItem(storageKey);
-    if (response === null && !persist) {
-      response = await (session
-        ? this.backends.persist.getItem(storageKey)
-        : this.backends.session.getItem(storageKey));
-    }
-    if (json && response) {
-      try {
-        response = JSON.parse(response) as TResponse;
-      } catch (error) {
-        if (!(error instanceof SyntaxError)) {
-          throw error;
-        }
-        // do nothing, return the string
-      }
-    }
-    return response;
-  };
-
   json = false;
-
   prefix = "";
-
-  // eslint-disable-next-line @typescript-eslint/require-await
-  removeItem = async (key: string) => {
-    // remove the key from temp, session and persist storage
-    const storageKey = this.formKey(key);
-    await this.backends.temp.removeItem(storageKey);
-    await this.backends.session.removeItem(storageKey);
-    return this.backends.persist.removeItem(storageKey);
-  };
-
-  setBackend = (storageBackends: Partial<StorageBackendOptions>) => {
-    this.backends = {
-      ...this.backends,
-      ...storageBackends,
-    };
-  };
-
-  setItem = async (
-    key: string,
-    value: unknown,
-    { json = this.json, persist = false, session = false } = {},
-    // eslint-disable-next-line @typescript-eslint/require-await
-  ) => {
-    // based on value of `persist` either store a value in temp or persist
-    if (value === null) return;
-    const storageKey = this.formKey(key);
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-    const saveValue = json ? JSON.stringify(value) : value;
-    const backend = this.getBackend(persist, session);
-    return backend.setItem(storageKey, saveValue as string);
-  };
-
   version: number | string = 1;
 
   constructor(env?: (typeof STORAGE_ENVIRONMENTS)[number]) {
@@ -127,6 +58,89 @@ export class AnyStorage {
           session: sessionStorage,
           temp: memoryStorage,
         };
+  }
+
+  async clear() {
+    this.backends.temp.clear();
+    if (this.prefix) {
+      for (const key of Object.keys(this.backends.session)) {
+        await this.backends.session.removeItem(key);
+      }
+      for (const key of Object.keys(this.backends.persist)) {
+        await this.backends.persist.removeItem(key);
+      }
+    }
+  }
+
+  getBackend({ persist, session }: GetBackendOptions = {}) {
+    if (persist) {
+      return this.backends.persist;
+    }
+    if (session) {
+      return this.backends.session;
+    }
+    return this.backends.temp;
+  }
+
+  getItem<TResponse = object>(
+    key: string,
+    options: GetItemOptions & { json: true },
+  ): Promise<TResponse | null>;
+
+  getItem<TResponse = string>(
+    key: string,
+    options?: GetItemOptions,
+  ): Promise<TResponse | null>;
+
+  async getItem<TResponse = object | string>(
+    key: string,
+    { json = this.json, persist, session }: GetItemOptions = {},
+  ): Promise<TResponse | null | string> {
+    const storageKey = this.formKey(key);
+    const backend = this.getBackend({ persist, session });
+    let response: TResponse | null | string = await backend.getItem(storageKey);
+    if (response === null && !persist) {
+      response = await (session
+        ? this.backends.persist.getItem(storageKey)
+        : this.backends.session.getItem(storageKey));
+    }
+    if (json && response) {
+      try {
+        response = JSON.parse(response) as TResponse;
+      } catch (error) {
+        if (!(error instanceof SyntaxError)) {
+          throw error;
+        }
+        // in case of syntax error return string
+      }
+    }
+    return response;
+  }
+
+  async removeItem(key: string) {
+    // remove the key from all of temp, session and persist storage
+    const storageKey = this.formKey(key);
+    await this.backends.temp.removeItem(storageKey);
+    await this.backends.session.removeItem(storageKey);
+    return this.backends.persist.removeItem(storageKey);
+  }
+
+  setBackend(storageBackends: Partial<StorageBackendOptions>) {
+    this.backends = {
+      ...this.backends,
+      ...storageBackends,
+    };
+  }
+  async setItem(
+    key: string,
+    value: unknown,
+    { json = this.json, persist = false, session = false } = {},
+  ) {
+    if (value === null) return;
+    const storageKey = this.formKey(key);
+    const saveValue = json ? JSON.stringify(value) : value;
+    const backend = this.getBackend({ persist, session });
+    return await backend.setItem(storageKey, saveValue as string);
   }
 }
 
