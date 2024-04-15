078e9f75a8095394583e74d65664eb5b22c95e62
Arvind Tiwari <tiwariav@gmail.com>
2024-04-15 15:23:47 +0530

    refactor: move tooltip parts in single file

--- src/components/atoms/Tooltip/Tooltip.tsx
+++ src/components/atoms/Tooltip/Tooltip.tsx
@@ -1,22 +1,79 @@
-import type { ReactNode } from "react";
+import type { FloatingContext } from "@floating-ui/react";
+import type {
+  CSSProperties,
+  ComponentProps,
+  MouseEventHandler,
+  ReactNode,
+  RefObject,
+} from "react";
 
-import { FloatingPortal } from "@floating-ui/react";
+import { FloatingArrow, FloatingPortal } from "@floating-ui/react";
+import { clsx } from "clsx";
+import { cloneElement, isValidElement } from "react";
 
+import type { TriggerOptions, UseTooltipOptions } from "./utils.js";
+
+import * as styles from "./tooltip.module.css";
 import {
-  TooltipBody,
-  type TooltipCommonProps,
-  TooltipTrigger,
-} from "./Parts.js";
-import {
-  type TriggerOptions,
-  type UseTooltipOptions,
+  ARROW_HEIGHT,
+  ARROW_WIDTH,
   useContextInteractions,
   useTooltipProps,
 } from "./utils.js";
 
-export interface TooltipProps extends TooltipCommonProps, UseTooltipOptions {
-  animate?: boolean;
+export interface TooltipCommonProps {
+  children?: ReactNode;
   className?: string;
+  onClick?: MouseEventHandler;
+}
+
+export interface TooltipTriggerProps extends TooltipCommonProps {
+  getReferenceProps: (
+    userProps?: ComponentProps<"button">,
+  ) => Record<string, unknown>;
+  referenceRef: (node: HTMLButtonElement | null) => void;
+}
+
+export function TooltipTrigger({
+  children,
+  className,
+  getReferenceProps,
+  onClick,
+  referenceRef,
+}: TooltipTriggerProps) {
+  if (
+    isValidElement<{
+      className: string;
+      onClick?: MouseEventHandler;
+    }>(children)
+  ) {
+    const childProps = children.props;
+    return cloneElement(children, {
+      className: clsx(styles.reference, className, children.props.className),
+      ...getReferenceProps({
+        ...childProps,
+        onClick: (event) => {
+          childProps.onClick?.(event);
+          onClick?.(event);
+        },
+        ref: referenceRef,
+      }),
+    });
+  }
+  return (
+    <button
+      className={clsx(styles.reference, className)}
+      {...getReferenceProps({
+        onClick,
+        ref: referenceRef,
+      })}
+    >
+      {children}
+    </button>
+  );
+}
+
+interface TooltipBodySharedProps {
   innerClassNames?: {
     arrow?: string;
     floating?: string;
@@ -24,6 +81,75 @@ export interface TooltipProps extends TooltipCommonProps, UseTooltipOptions {
     title?: string;
   };
   isPopover?: boolean;
+}
+
+interface TooltipBodyProps extends TooltipBodySharedProps {
+  animate?: boolean;
+  arrowRef?: RefObject<SVGSVGElement>;
+  context: FloatingContext;
+  floatingRef: (node: HTMLDivElement | null) => void;
+  floatingStyles: CSSProperties;
+  getFloatingProps: (
+    userProps?: ComponentProps<"div">,
+  ) => Record<string, unknown>;
+  showArrow?: boolean;
+  showBody?: boolean;
+  title: ReactNode;
+  transitionStyles: CSSProperties;
+}
+
+export function TooltipBody({
+  animate,
+  arrowRef,
+  context,
+  floatingRef,
+  floatingStyles,
+  getFloatingProps,
+  innerClassNames,
+  isPopover,
+  showArrow,
+  showBody,
+  title,
+  transitionStyles,
+}: TooltipBodyProps) {
+  return (
+    showBody && (
+      <div
+        className={clsx(styles.floating, innerClassNames?.floating)}
+        {...getFloatingProps({
+          ref: floatingRef,
+          style: floatingStyles,
+        })}
+      >
+        <div
+          className={clsx(styles.title, innerClassNames?.title, {
+            [styles.isPlain]: !isPopover,
+          })}
+          style={animate ? transitionStyles : {}}
+        >
+          {title}
+        </div>
+        {showArrow && (
+          <FloatingArrow
+            className={clsx(styles.arrow, innerClassNames?.arrow)}
+            context={context}
+            height={ARROW_HEIGHT}
+            ref={arrowRef}
+            width={ARROW_WIDTH}
+          />
+        )}
+      </div>
+    )
+  );
+}
+
+export interface TooltipProps
+  extends TooltipCommonProps,
+    UseTooltipOptions,
+    TooltipBodySharedProps {
+  animate?: boolean;
+  className?: string;
+
   portal?: boolean;
   title: ReactNode;
   trigger?: TriggerOptions;
