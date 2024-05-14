78e4cb1a4339b0785ba04a723a2bfc95253a8756
Arvind Tiwari <tiwariav@gmail.com>
2024-05-14 05:41:56 +0530

    refactor: update stylelint to v16 and add patch for pnp

--- .stylelintrc.cjs
+++ .stylelintrc.cjs
@@ -1,12 +1,14 @@
+const a = require("stylelint");
+
 module.exports = {
   extends: "@tiwariav/stylelint-config",
   rules: {
+    "csstools/media-use-custom-media": null,
     "csstools/value-no-unknown-custom-properties": [
       true,
       {
         importFrom: ["src/styles/base.css"],
       },
     ],
-    "csstools/media-use-custom-media": null,
   },
 };
